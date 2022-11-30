a3af2f78e8c75695a2b5fa9dea77ee1109087628
amin <94103247+amin-rahmani@users.noreply.github.com>
2022-11-30 16:47:48 +0330

    some psudo code for js

---
+++ script.js
@@ -0,0 +1,13 @@
+// first build the abilities
+//accept user Inputs,number,operator,and number
+//store inputs
+//recognize inputs and perform calculations
+//should accept decimal numbers
+//show the result in screen
+
+//optional features:
+//should accept longer arithmetic operations.
+// display all input as it is being entered
+// store previous total as start of next operation.
+//clear button should clear all entreis
+//should prevent invalid input(operators next to each other,two decimal points)
