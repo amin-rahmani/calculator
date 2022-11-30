387c796f906f5194257e0c9f228cdc514a08cd98
amin <94103247+amin-rahmani@users.noreply.github.com>
2022-11-30 12:45:25 +0330

    js level 1

--- script.js
+++ script.js
@@ -11,3 +11,31 @@
 // store previous total as start of next operation.
 //clear button should clear all entreis
 //should prevent invalid input(operators next to each other,two decimal points)
+
+const keys = document.querySelector('.calculator-buttons');
+keys.addEventListener('click',event =>{
+    const {target} = event;
+    const {value} = target;
+    if(!target.matches('button')){
+        return;
+    }else{
+        //pass value to parse method
+        //do this first and see the difference
+        // console.log(event);
+        // console.log(target);
+        // console.log(value);
+        calculator.parseInput(value)
+    }
+})
+//let's build calculator factory
+const calculator = {
+    displayText :'0',
+    prevTotal : null,
+
+    parseInput(value){
+        if(this.displayText === '0'){
+            this.displayText = ''
+        }
+        
+    }
+}
